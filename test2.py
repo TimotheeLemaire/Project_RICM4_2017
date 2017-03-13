813d458b6525b4855141b0cd0051b004b3257bc1
Unknown <lemaire.timothee@gmail.com>
2017-03-13 14:53:53 +0100

    *

--- test2.py
+++ test2.py
@@ -1,10 +1,12 @@
 from execo import *
-hosts = [ "192.168.99.101", "192.168.99.102", "192.168.99.103", "192.168.99.104" ]
+hosts = [ Host("192.168.99.100", user='docker'), Host("192.168.99.101", user='docker')]
 targets = list(reversed(hosts))
-servers = Remote("./123Soleil", hosts)
+servers = Remote("nc -l -p 6543 > /dev/null", hosts,{'scp_options': ('-o', 'PasswordAuthentication=no',)})
+clients = Remote("dd if=/dev/zero bs=50000 count=125 | nc -q 0 {{targets}} 6543", hosts)
 with servers.start():
   sleep(1)
-  servers.run()
+  clients.run()
+  servers.wait()
 print Report([ servers, clients ]).to_string()
 for s in servers.processes + clients.processes:
   print "%s\nstdout:\n%s\nstderr:\n%s" % (s, s.stdout, s.stderr)
