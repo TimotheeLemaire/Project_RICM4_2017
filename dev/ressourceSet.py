9e49776f3bcc669958b56dabc500d7bbd2a342d9
nicomy <nicomy_68@hotmail.fr>
2017-03-14 09:55:39 +0100

    hash en ruby = dict() en python

--- dev/ressourceSet.py
+++ dev/ressourceSet.py
@@ -1,13 +1,29 @@
 
-
+#A Resource maps a computational resource to an object which keeps 
+#Certains characteritics such as type, name, gateway.
 class Resource():
 	"""docstring for Resource"""
+	self.Type #type of the resource
+	self.properties #properties of the resources
 
 
-	def __init__(typ, properties=None , name = None):
-		self.typ = typ
-		self.properties = 
-		
+    # Creates a new Resource Object.
+    # @param [type] type of the source
+    # @param [properties] object property
+    # @param [String] String name
+	# @return [resource] Resource Object
+	def __init__(typ, prop=None , name = None):
+		self.Type = types
+		self.properties = dict() 
+
+		if prop: #test si prop est vide 
+			#----replaces the contents of @properties hash with
+            #----contents of 'properties' hash
+        	self.properties = prop  
+
+		if name :
+			self.properties["name"] = name 
+
 
 
 
